3e6dbbc8655b6c6523fdc10437ef80246914fd7c
Michael Donovan <michael@mdonovan.dev>
2023-08-20 16:48:03 -0500

    init nix flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Flake for mbdsp";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils }@inputs:
+    utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          packages = with pkgs;{
+            pkg = pkgs.stdenv.mkDerivation {
+              pname = "mbdsp";
+              version = "0.1.0";
+              src = ./.;
+
+              propagatedBuildInputs = [
+                cmake
+              ];
+
+              meta = with lib;
+                {
+                  description = "mbdsp";
+                  license = licenses.gpl3;
+                  platforms = platforms.all;
+                  maintainers = with maintainers; [ michaeldonovan ];
+                };
+            };
+          };
+
+          defaultPackage = self.packages.${system}.pkg;
+        });
+}
+
